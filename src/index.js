608fc93775dd1c755a6cf5d092adada43e95121c
Denis Nemtsov <denis.infospb@gmial.com>
2023-06-27 01:02:34 +0300

    Release 0

---
+++ src/index.js
@@ -0,0 +1,122 @@
+import './index.html';
+import './index.scss';
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+
+// swipper //
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+ 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+});
+
+var swiper = new Swiper(".cardSwiper", {
+  slidesPerView: 2,
+  spaceBetween: 105,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    
+  },
+});
+
+
+// // ////////
+let i = 0
+    document.addEventListener('click', function(e) {
+        const target = e.target
+        
+        const wrapper = target.parentNode.previousElementSibling   
+        const read = target.parentNode 
+        if (target.className == 'about__unwrap-brandlist' ||
+          target.className == 'about__read-brandlist' ||
+          target.className == 'arrow-brandlist') {
+          
+          wrapper.classList.add('btn-active')  
+          read.children[0].style = "rotate: 180deg"   
+          read.children[1].style = "rotate: 180deg"   
+          read.children[2].textContent = "Скрыть"   
+          i++  
+          
+          if (i % 2 == 0) {
+            wrapper.classList.remove('btn-active')
+            read.children[0].style.rotate = "360deg"   
+            read.children[1].style = "rotate: 360deg"
+            read.children[2].textContent = "Показать все"
+            
+          }
+        }
+    });
+
+//////////////////SIDEBAR//////////////// 
+
+
+const sidebar = document.querySelector('.left-sidebar')
+const wrapper = document.querySelector('.wrapper') 
+const application = document.querySelector('.application')
+const callbac = document.querySelector('.callback')
+const exitButtons = document.querySelectorAll('.icon8')
+
+
+
+document.addEventListener('click', function(e) {
+  const target = e.target
+  
+  ///////SIDEBAR ///////////////
+ 
+  if (target.className.includes('icon1')) {
+    sidebar.classList.add('sidebar-active')
+  } 
+  if (target.closest('.wrapper') &&
+    !target.className.includes('icon1') ||
+    target == exitButtons[0]) {
+      sidebar.classList.remove('sidebar-active')
+      wrapper.classList.remove('opacity')  
+  } 
+  /////////////////////////////// 
+  
+  if (target.className.includes('icon2')) {
+    application.classList.add('applictation-active')
+  }
+  if (target.closest('.wrapper') &&
+    !target.className.includes('icon2') ||
+    target == exitButtons[1]) {
+      wrapper.classList.remove('opacity')  
+      application.classList.remove('applictation-active')
+  } 
+
+// ///////////////////////////
+
+  if (target.className.includes('icon3')) {
+      callbac.classList.add('applictation-active')
+  }
+  if (target.closest('.wrapper') &&
+    !target.className.includes('icon3') ||
+      target == exitButtons[2]) {
+      wrapper.classList.remove('opacity')  
+      callbac.classList.remove('applictation-active')
+  }
+  
+  if (target.className.includes('icon1') ||
+    target.className.includes('icon2') ||
+    target.className.includes('icon3')) {
+    wrapper.classList.add('opacity')
+  }
+});
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    open: true,
+    hot: true,
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              }
+            }
+
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[ext]'
+        }
+      },
+      
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+    ],
+  },
+};
